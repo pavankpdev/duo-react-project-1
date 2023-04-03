fb6b284a09ed39735b695e33bde0e0a3b25feaa8
pavanleton <pavankumarkpkp@gmail.com>
2023-04-03 21:30:12 +0530

    setup React router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
 function App() {
 
 
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Welcome to Rick and Morty API</h1>
+            <button>
+                Get started
+            </button>
+        </>
+    )
+}
+
+export default Home;
