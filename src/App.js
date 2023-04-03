0ed216519f627442d0d04a2a2c20b08c581e8827
pavanleton <pavankumarkpkp@gmail.com>
2023-04-03 21:35:12 +0530

    add dashboard route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/home'
+import Dashboard from './pages/dashaboard'
 function App() {
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,17 @@
+import {useNavigate} from 'react-router-dom'
+
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/dashboard')
+    }
+
     return (
         <>
             <h1>Welcome to Rick and Morty API</h1>
-            <button>
+            <button onClick={handleClick}>
                 Get started
             </button>
         </>
